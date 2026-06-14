ea692bc0b09e73ff12aa87e8172e9593012e7179
Fey Xie <feyxiexzf@gmail.com>
2026-06-14 10:10:29 +0800

    fix PreventXposedDetection toggle

--- app/src/main/java/dev/ujhhgtg/wekit/hooks/items/system/PreventXposedDetection.kt
+++ app/src/main/java/dev/ujhhgtg/wekit/hooks/items/system/PreventXposedDetection.kt
@@ -7,32 +7,28 @@ import dev.ujhhgtg.wekit.dexkit.dsl.dexMethod
 import dev.ujhhgtg.wekit.hooks.core.HookItem
 import dev.ujhhgtg.wekit.hooks.core.SwitchHookItem
 import dev.ujhhgtg.wekit.ui.content.AlertDialogContent
+import dev.ujhhgtg.wekit.ui.content.TextButton
 import dev.ujhhgtg.wekit.ui.utils.showComposeDialog
 import dev.ujhhgtg.wekit.utils.HostInfo
-import org.luckypray.dexkit.DexKitBridge
 
 @HookItem(name = "禁止微信检测 Xposed", categories = ["系统与隐私"], description = "防止微信检测 Xposed 框架是否存在")
-object DisableXposedDetection : SwitchHookItem(), IResolvesDex {
+object PreventXposedDetection : SwitchHookItem(), IResolvesDex {
 
-    private val methodCheckStackTraceElements by dexMethod()
+    private val methodCheckStackTraceElements by dexMethod {
+        searchPackages("com.tencent.mm.app")
+        matcher {
+            usingEqStrings(
+                "de.robv.android.xposed.XposedBridge",
+                "com.zte.heartyservice.SCC.FrameworkBridge"
+            )
+        }
+    }
 
     override fun onEnable() {
         if (methodCheckStackTraceElements.isPlaceholder) return
 
         methodCheckStackTraceElements.hookBefore {
             result = false
-        }
-    }
-
-    override fun resolveDex(dexKit: DexKitBridge) {
-        methodCheckStackTraceElements.find(dexKit, allowFailure = true) {
-            searchPackages("com.tencent.mm.app")
-            matcher {
-                usingEqStrings(
-                    "de.robv.android.xposed.XposedBridge",
-                    "com.zte.heartyservice.SCC.FrameworkBridge"
-                )
-            }
         }
     }
 
@@ -43,10 +39,12 @@ object DisableXposedDetection : SwitchHookItem(), IResolvesDex {
                     title = { Text("禁止微信检测 Xposed") },
                     text = {
                         Text("Google Play 版微信无此检测, 开启可能导致闪退, 已关闭功能!")
-                    })
+                    },
+                    confirmButton = { TextButton(onDismiss) { Text("取消") }})
             }
+            return false
         }
 
-        return false
+        return true
     }
 }
